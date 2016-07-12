d5cd7c5ee67897f9f07f365432221338c0b88be5
Tom Marsh <ts.marsh@gmail.com>
2016-07-12 07:47:54 -0400

    Architype

---
+++ src/main/java/com/tailoredshapes/App.java
@@ -0,0 +1,13 @@
+package com.tailoredshapes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
